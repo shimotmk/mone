2f2f62a1fdafb904d3b349ef5d09e2f4e3d2ab8b
shimotmk <shimotmk1104@gmail.com>
2025-01-04 06:24:28 +0900

    format css

--- src/format-library/block-toolbar/index.js
+++ src/format-library/block-toolbar/index.js
@@ -9,7 +9,12 @@ import clsx from 'clsx';
 import { __ } from '@wordpress/i18n';
 import { BlockFormatControls } from '@wordpress/block-editor';
 import { registerFormatType } from '@wordpress/rich-text';
-import { ToolbarItem, Slot, DropdownMenu } from '@wordpress/components';
+import {
+	ToolbarItem,
+	Slot,
+	DropdownMenu,
+	ToolbarGroup,
+} from '@wordpress/components';
 import { Icon } from '@wordpress/icons';
 
 import { blockCategoryIcon } from '../../icons';
@@ -21,62 +26,73 @@ const POPOVER_PROPS = {
 const BlockToolbar = () => {
 	return (
 		<BlockFormatControls>
-			<Slot name={ `RichText.ToolbarControls.moneMenu` }>
-				{ ( fills ) => {
-					if ( ! fills.length ) {
-						return null;
-					}
+			<div className="block-editor-format-toolbar mone-format-toolbar">
+				<ToolbarGroup>
+					<Slot name={ `RichText.ToolbarControls.moneMenu` }>
+						{ ( fills ) => {
+							if ( ! fills.length ) {
+								return null;
+							}
 
-					const allProps = fills.map( ( [ { props } ] ) => props );
-					const hasActive = allProps.some(
-						( { isActive } ) => isActive
-					);
+							const allProps = fills.map(
+								( [ { props } ] ) => props
+							);
+							const hasActive = allProps.some(
+								( { isActive } ) => isActive
+							);
 
-					const order = [
-						__( 'Icon', 'mone' ),
-						__( 'Inline Font Size', 'mone' ),
-						__( 'Text Gradient', 'mone' ),
-						__( 'Background Gradient', 'mone' ),
-						__( 'Stroke', 'mone' ),
-						__( 'Bold', 'mone' ),
-						__( 'Reset Formatting', 'mone' ),
-						__( 'Remove Format', 'mone' ),
-					];
-					const sortedFills = fills.sort( ( a, b ) => {
-						const titleA = a[ 0 ].props.title;
-						const titleB = b[ 0 ].props.title;
-						return (
-							order.indexOf( titleA ) - order.indexOf( titleB )
-						);
-					} );
-					const controls = sortedFills.map(
-						( [ { props } ] ) => props
-					);
+							const order = [
+								__( 'Icon', 'mone' ),
+								__( 'Inline Font Size', 'mone' ),
+								__( 'Text Gradient', 'mone' ),
+								__( 'Background Gradient', 'mone' ),
+								__( 'Stroke', 'mone' ),
+								__( 'Bold', 'mone' ),
+								__( 'Reset Formatting', 'mone' ),
+								__( 'Remove Format', 'mone' ),
+							];
+							const sortedFills = fills.sort( ( a, b ) => {
+								const titleA = a[ 0 ].props.title;
+								const titleB = b[ 0 ].props.title;
+								return (
+									order.indexOf( titleA ) -
+									order.indexOf( titleB )
+								);
+							} );
+							const controls = sortedFills.map(
+								( [ { props } ] ) => props
+							);
 
-					return (
-						<ToolbarItem>
-							{ ( toggleProps ) => (
-								<DropdownMenu
-									icon={ <Icon icon={ blockCategoryIcon } /> }
-									label={ __( 'Mone Menu', 'mone' ) }
-									toggleProps={ {
-										...toggleProps,
-										className: clsx(
-											toggleProps.className,
-											{ 'is-pressed': hasActive }
-										),
-										description: __(
-											'Displays more mone tools'
-										),
-									} }
-									controls={ controls }
-									popoverProps={ POPOVER_PROPS }
-								/>
-							) }
-						</ToolbarItem>
-					);
-				} }
-			</Slot>
+							return (
+								<ToolbarItem>
+									{ ( toggleProps ) => (
+										<DropdownMenu
+											icon={
+												<Icon
+													icon={ blockCategoryIcon }
+												/>
+											}
+											label={ __( 'Mone Menu', 'mone' ) }
+											toggleProps={ {
+												...toggleProps,
+												className: clsx(
+													toggleProps.className,
+													{ 'is-pressed': hasActive }
+												),
+												description: __(
+													'Displays more mone tools'
+												),
+											} }
+											controls={ controls }
+											popoverProps={ POPOVER_PROPS }
+										/>
+									) }
+								</ToolbarItem>
+							);
+						} }
+					</Slot>
+				</ToolbarGroup>
+			</div>
 		</BlockFormatControls>
 	);
 };
