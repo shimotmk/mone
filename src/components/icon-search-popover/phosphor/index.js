7e700541ebc74e68652b14cc1e016fcc8b31db02
shimotmk <shimotmk1104@gmail.com>
2025-01-11 07:34:53 +0900

    up

--- src/components/icon-search-popover/phosphor/index.js
+++ src/components/icon-search-popover/phosphor/index.js
@@ -6,90 +6,86 @@ import { PhosphorIconList } from '../icon-list/phosphor-icons';
 /**
  * WordPress dependencies
  */
-import { Button, ButtonGroup, TabPanel } from '@wordpress/components';
+import {
+	Button,
+	ButtonGroup,
+	__experimentalToggleGroupControl as ToggleGroupControl,
+	__experimentalToggleGroupControlOption as ToggleGroupControlOption,
+} from '@wordpress/components';
 import { useState } from '@wordpress/element';
 import { Icon } from '@wordpress/icons';
 
 export const Phosphor = ( { iconName, onChange } ) => {
 	const parts = iconName?.includes( '_' ) ? iconName.split( '_' ) : [];
-	const _iconType = parts[ 1 ] || '';
+	const _iconType = parts[ 1 ] || 'thin';
 	const iconNamePart = parts[ 2 ] || '';
 	const [ iconType, setIconType ] = useState( _iconType );
 
 	return (
 		<>
-			<TabPanel
-				className="mone-icon-type-tab"
-				activeClass="is-active"
-				tabs={ [
-					{
-						name: 'thin',
-						title: 'thin',
-					},
-					{
-						name: 'light',
-						title: 'light',
-					},
-					{
-						name: 'bold',
-						title: 'bold',
-					},
-				] }
-				initialTabName={ iconType }
-				onSelect={ ( value ) => setIconType( value ) }
-			>
-				{ () => {
-					return (
-						<>
-							<div className="mone-icon-tab-content">
-								<ButtonGroup>
-									{ PhosphorIconList.map(
-										( iconObj, idx ) => {
-											return (
-												<Button
-													className="mone-icon-button fi"
-													key={ idx }
-													variant={
-														iconObj.iconList.find(
-															( icon ) =>
-																icon.type ===
-																_iconType
-														) &&
-														iconObj.name ===
-															iconNamePart
-															? 'primary'
-															: undefined
-													}
-													onClick={ () => {
-														const newIcon =
-															iconObj.name ===
-															iconNamePart
-																? ''
-																: `Phosphor_${ iconType }_${ iconObj.name }`;
-														onChange( newIcon );
-													} }
-													label={ iconObj.name }
-												>
-													<Icon
-														icon={
-															iconObj.iconList.find(
-																( icon ) =>
-																	icon.type ===
-																	iconType
-															)?.svgHtml
-														}
-														size={ 24 }
-													/>
-												</Button>
-											);
+			<div className="mone-icon-type-tab">
+				<div className="mone-icon-tab-content">
+					<div className="mone-icon-type-toggle-wrapper">
+						<div className="mone-icon-type-toggle">
+							<ToggleGroupControl
+								value={ iconType }
+								isBlock
+								__nextHasNoMarginBottom
+								__next40pxDefaultSize
+								onChange={ ( value ) => setIconType( value ) }
+							>
+								<ToggleGroupControlOption
+									value="thin"
+									label="Thin"
+								/>
+								<ToggleGroupControlOption
+									value="light"
+									label="Light"
+								/>
+								<ToggleGroupControlOption
+									value="bold"
+									label="Bold"
+								/>
+							</ToggleGroupControl>
+						</div>
+					</div>
+					<ButtonGroup>
+						{ PhosphorIconList.map( ( iconObj, idx ) => {
+							return (
+								<Button
+									className="mone-icon-button fi"
+									key={ idx }
+									variant={
+										iconObj.iconList.find(
+											( icon ) => icon.type === _iconType
+										) && iconObj.name === iconNamePart
+											? 'primary'
+											: undefined
+									}
+									onClick={ () => {
+										const newIcon =
+											iconObj.name === iconNamePart
+												? ''
+												: `Phosphor_${ iconType }_${ iconObj.name }`;
+										onChange( newIcon );
+									} }
+									label={ iconObj.name }
+								>
+									<Icon
+										icon={
+											iconObj.iconList.find(
+												( icon ) =>
+													icon.type === iconType
+											)?.svgHtml
 										}
-									) }
-								</ButtonGroup>
-							</div>
-						</>
-					);
-				} }
-			</TabPanel>
+										size={ 24 }
+									/>
+								</Button>
+							);
+						} ) }
+					</ButtonGroup>
+				</div>
+			</div>
 		</>
 	);
 };
