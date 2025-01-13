e44fbc023f864c38784f1f5fe3ad393a6c99cfa6
shimotmk <shimotmk1104@gmail.com>
2025-01-13 11:50:24 +0900

    up

--- src/components/icon-search-popover/custom-icon/index.js
+++ src/components/icon-search-popover/custom-icon/index.js
@@ -3,30 +3,48 @@
  */
 import { __ } from '@wordpress/i18n';
 import { TextareaControl, Button } from '@wordpress/components';
-import { useState } from '@wordpress/element';
+import { useEffect, useState, renderToString } from '@wordpress/element';
 import { Icon } from '@wordpress/icons';
 
 import { blockCategoryIcon } from '../../../icons';
+import { parseIcon } from '../utils/parse-icon';
+import { ReactIcon, isCustomIcon } from '../ReactIcon';
 
-export const CustomIcon = ( { iconSVG, onChange } ) => {
+export const CustomIcon = ( { iconName, iconSVG, onChange } ) => {
 	const [ preSvgIcon, setPreSvgIcon ] = useState( iconSVG );
+
+	useEffect( () => {
+		let svgContent;
+		if ( iconName && isCustomIcon( iconName ) ) {
+			svgContent = iconSVG;
+		} else if ( iconName ) {
+			svgContent = renderToString( <ReactIcon iconName={ iconName } /> );
+		} else {
+			svgContent = renderToString( <ReactIcon iconName="FaWordpress" /> );
+		}
+		setPreSvgIcon( svgContent );
+	}, [ iconName, iconSVG ] );
+
+	const iconParse = parseIcon( preSvgIcon );
+	const isSVG = !! iconParse?.props;
 
 	return (
 		<>
 			<div className="mone-icon-type-tab">
-				<div className="mone-icon-tab-content">
-					{ preSvgIcon ? (
-						<div
-							className="mone-preview-svg-icon"
-							dangerouslySetInnerHTML={ {
-								__html: preSvgIcon,
-							} }
-						/>
+				<div className="mone-icon-tab-content--custom">
+					{ isSVG ? (
+						<div className="mone-preview-svg-icon">
+							{ parseIcon( preSvgIcon ) }
+						</div>
 					) : (
-						<Icon
-							className="mone-preview-svg-icon"
-							icon={ blockCategoryIcon }
-						/>
+						<div className="mone-preview-svg-icon default-icon">
+							<Icon icon={ blockCategoryIcon } />
+						</div>
+					) }
+					{ ! isSVG && (
+						<div className="mone-preview-svg-text">
+							{ __( 'Invalid SVG code', 'mone' ) }
+						</div>
 					) }
 					<TextareaControl
 						className="mone-preview-svg-textarea"
@@ -37,18 +55,21 @@ export const CustomIcon = ( { iconSVG, onChange } ) => {
 						placeholder={ __( 'Svg code is here', 'mone' ) }
 						value={ preSvgIcon }
 					/>
+					<div className="mone-preview-svg-button-area">
+						<Button
+							disabled={ ! isSVG && ! preSvgIcon }
+							variant="primary"
+							onClick={ () => {
+								onChange( {
+									iconType: 'custom',
+									iconSVG: preSvgIcon,
+								} );
+							} }
+						>
+							{ __( 'Insert custom icon', 'mone' ) }
+						</Button>
+					</div>
 				</div>
-				<Button
-					variant="primary"
-					onClick={ () => {
-						onChange( {
-							iconType: 'custom',
-							iconSVG: preSvgIcon,
-						} );
-					} }
-				>
-					{ __( 'Insert custom icon', 'mone' ) }
-				</Button>
 			</div>
 		</>
 	);
