382718ce9025c01971aad022d65d8fa2075f5f8d
shimotmk <shimotmk1104@gmail.com>
2025-02-23 11:37:47 +0900

    u

--- src/block-hook/group/dialog/inline/constants.js
+++ src/block-hook/group/dialog/inline/constants.js
@@ -51,6 +51,46 @@ export function createDialogBlock( id, name ) {
 					},
 				},
 				[
+					createBlock(
+						'core/group',
+						{
+							metadata: {
+								name: __( 'Dialog close', 'mone' ),
+							},
+							style: {
+								position: {
+									type: 'sticky',
+									top: '1.25rem',
+									right: '0px',
+								},
+							},
+							layout: {
+								type: 'constrained',
+							},
+						},
+						[
+							createBlock(
+								'mone/icons',
+								{
+									align: 'full',
+									className: 'dialog-close-button',
+									layout: {
+										type: 'flex',
+										justifyContent: 'right',
+									},
+								},
+								[
+									createBlock( 'mone/icon', {
+										width: '1.25rem',
+										height: '1.25rem',
+										tagName: 'button',
+										iconSVG:
+											'<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path></svg>',
+									} ),
+								]
+							),
+						]
+					),
 					createBlock( 'core/paragraph', {
 						placeholder: __( 'Dialog contents is here!', 'mone' ),
 					} ),
